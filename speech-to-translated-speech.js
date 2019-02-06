bf52d1280f0e2798e3bf363da0c3157b75197b81
Benoit <benoitdavid350@gmail.com>
2019-02-06 15:45:57 +0100

    intégration langages

--- speech-to-translated-speech.js
+++ speech-to-translated-speech.js
@@ -4,35 +4,14 @@ const trans = require("./libs/translation.js");
 
 const async = require("async");
 
-async function test ()
-{
+async function vocalTranslator(langage){
     let texte = await asr.transcribeShortFile('./audio/tts.wav');
     texte = JSON.stringify(texte);
     let json = JSON.parse(texte);
     let transcription = json[0].alternatives[0].transcript;
     console.log(transcription);
-    translated = await trans.translate(transcription); //["transcript"]
+    translated = await trans.translate(transcription,langage);
     let resultat = await tts.speakToFile(translated, './audio/tts.wav');
 }
 
 //test()
-
-/*async function api(){
-  json = {
-    "Deutsch (Deutschland)": "de-DE",
-    "English (Great Britain)": "en-GB",
-    "English (United States)": "en-US",
-    "Español (España)": "es-ES",
-    "Français (France)": "fr-FR",
-    "Italiano (Italia)": "it-IT",
-    "Nederlands (Nederland)": "nl-NL",
-    "Português (Portugal)": "pt-PT",
-    "Română (România)": "ro-RO",
-    "Slovenčina (Slovensko)": "sk-SK",
-    "Русский (Россия)": "ru-RU",
-    "한국어 (대한민국)": "ko-KR",
-    "國語 (台灣)": "zh-TW",
-    "日本語（日本)": "ja-JP"
-  }
-  json = JSON.parse(json);
-}*/
